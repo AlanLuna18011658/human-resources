24ef34729d4c03b6329f6122e3f12f445f4d35aa
imac <za18011658@zapopan.tecmm.edu.mx>
2023-11-20 23:51:16 -0600

    Actualización

--- ashure/rotacion_personal.php
+++ ashure/rotacion_personal.php
@@ -29,7 +29,7 @@
    <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Ashure - Registro Empleado</title>
+      <title>Ashure - Rotación de personal</title>
       <link rel="icon" href="ashure.ico">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
       <style>
